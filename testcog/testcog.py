2c64a4b96eddc782826b9a7b5f906f2a57a853ae
SabishiiAisu <mjsims24@gmail.com>
2019-11-26 10:46:10 -0500

    Update testcog.py

--- testcog/testcog.py
+++ testcog/testcog.py
@@ -6,31 +6,29 @@ class TestCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.owner = bot.get_user(136242970000097280)
-        # self.list = []
+        self.mylist = [1,2,3,4]
     
     @commands.command()
     async def test(self, ctx):
         """This does stuff!"""
         # Your code will go here
         author = ctx.author
-        await author.send(self.owner.name)
+        await author.send(self.mylist)
 
-    """
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         author = self.bot.get_user(payload.user_id)
-        if (author.bot):
+        if author.bot or author != self.owner):
             return
-        channel = self.bot.get_channel(payload.channel_id)
+        # channel = self.bot.get_channel(payload.channel_id)
         # self.list.append(self.bot.get_emoji(payload.emoji_id).name)
-        await self.bot.send_message(self.owner, 'I see you!')
+        await author.send('I see you!')
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
         author = self.bot.get_user(payload.user_id)
-        if (author.bot):
+        if (author.bot or author != self.owner):
             return
-        channel = self.bot.get_channel(payload.channel_id)
+        # channel = self.bot.get_channel(payload.channel_id)
         # self.list.remove(self.bot.get_emoji(payload.emoji_id).name)
-        await self.bot.send_message(self.owner, 'Stop hiding!')
-    """
+        await author.send('Stop hiding!')

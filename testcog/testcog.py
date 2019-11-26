a1f36d59b619a960103de8a7074ba9cba048cead
SabishiiAisu <mjsims24@gmail.com>
2019-11-26 01:55:16 -0500

    Update testcog.py

--- testcog/testcog.py
+++ testcog/testcog.py
@@ -15,12 +15,18 @@ class TestCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
+        user = self.bot.get_user(payload.user_id)
+        if (user.bot):
+            return
         channel = self.bot.get_channel(payload.channel_id)
         self.list.append(self.bot.get_emoji(payload.emoji_id).name)
         await channel.send('I see you!')
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
+        user = self.bot.get_user(payload.user_id)
+        if (user.bot):
+            return
         channel = self.bot.get_channel(payload.channel_id)
         self.list.remove(self.bot.get_emoji(payload.emoji_id).name)
         await channel.send('Stop hiding!')

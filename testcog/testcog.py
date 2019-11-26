59ae9972dcaa543f37fd5a5b42eb23903c8ad4c4
SabishiiAisu <mjsims24@gmail.com>
2019-11-26 01:04:48 -0500

    Update testcog.py

--- testcog/testcog.py
+++ testcog/testcog.py
@@ -3,6 +3,9 @@ from redbot.core import commands
 class TestCog(commands.Cog):
     """My custom cog"""
 
+    def __init__(self, bot):
+        self.bot = bot
+    
     @commands.command()
     async def test(self, ctx):
         """This does stuff!"""
@@ -10,9 +13,9 @@ class TestCog(commands.Cog):
         await ctx.send("I can do stuff!")
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, ctx, reaction, user):
+    async def on_reaction_add(reaction, user):
         channel = reaction.message.channel
-        await ctx.send_message(channel, '{} has added {} to the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
+        await bot.send_message(channel, '{} has added {} to the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
 
     """
     @commands.Cog.listener()

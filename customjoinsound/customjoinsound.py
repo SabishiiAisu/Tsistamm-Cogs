70255156f338b455a438e84c7526ad3e1985dece
Tsistamm <mjsims24@gmail.com>
2020-02-16 21:56:32 -0500

    Subcommand testing.

--- customjoinsound/customjoinsound.py
+++ customjoinsound/customjoinsound.py
@@ -3,6 +3,11 @@ from redbot.core import commands
 class CustomJoinSound(commands.Cog):
     """My custom cog"""
 
-    @commands.command()
+    @commands.group()
     async def cjs(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Invalid subcommand!")
+
+    @cjs.command
+    async def setsound(self, ctx):
         await ctx.send("Something!")

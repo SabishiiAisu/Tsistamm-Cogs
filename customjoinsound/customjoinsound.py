f036ca918591df5160a3705735e604229fbac98c
Tsistamm <mjsims24@gmail.com>
2020-02-16 21:52:00 -0500

    Restructuring.

--- customjoinsound/customjoinsound.py
+++ customjoinsound/customjoinsound.py
@@ -3,22 +3,6 @@ from redbot.core import commands
 class CustomJoinSound(commands.Cog):
     """My custom cog"""
 
-    def __init__(self, bot):
-        self.bot = bot
-        self.owner = bot.get_user(136242970000097280)
-        self.mylist = []
-
     @commands.command()
-    async def cjs(ctx):
-        await ctx.send('Something!')
-
-    """
-    @commands.group()
-    async def cjs(ctx):
-        if ctx.invoked_subcommand is None:
-            await ctx.send('Invalid subcommand passed...')
-
-    @cjs.command()
-    async def setsound(ctx):
-        await ctx.send('Setting sound!')
-    """
+    async def cjs(self, ctx):
+        await ctx.send("Something!")
